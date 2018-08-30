2e766b60c6ccf5d8e5d84fe95e652392323efe33
grezar <grezar.dev@gmail.com>
2018-08-31 02:16:39 +0900

    Added test_helper.bash

---
+++ test/test_helper.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+TIDEL_ROOT=$(cd $(dirname $0)/.. ; pwd)
+TIDEL_PATH="${HOME}/.tidel"
+TIDEL_SCRIPTS_PATH="$TIDEL_PATH/scripts"
+SCRIPTS_NAME=tidel-test
+COUNT=0
+export COUNT=$COUNT
